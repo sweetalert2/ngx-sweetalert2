250bc31b0d7b8c70d2c2a4b8184a3d77c5fcbd61
Unknown <mtouverey@methodinthemadness.eu>
2017-01-23 22:46:29 +0100

    feat(SwalComponent): Add SwalComponent

---
+++ src/swal.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import swal, { SweetAlertOptions, SweetAlertType } from 'sweetalert2';
+
+@Component({
+    selector: 'swal',
+    template: ''
+})
+export class SwalComponent {
+    @Input() public type: SweetAlertType;
+    @Input() public title: string;
+    @Input() public text: string;
+    @Input() public html: string;
+    @Input() public options: SweetAlertOptions;
+
+    @Output() public confirm: EventEmitter<any> = new EventEmitter();
+    @Output() public cancel: EventEmitter<any> = new EventEmitter();
+
+    public show(): Promise<any> {
+        const options = Object.assign({
+            type: this.type,
+            title: this.title,
+            text: this.text,
+            html: this.html,
+        }, this.options);
+
+        const promise = swal(options);
+
+        promise.then(
+            (success) => this.confirm.emit(success),
+            (dismiss) => this.cancel.emit(dismiss)
+        );
+
+        return promise;
+    }
+}
