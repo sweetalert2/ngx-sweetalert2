10a6a20a5506181dd95684ca6f5aa0e79a020926
Unknown <mtouverey@methodinthemadness.eu>
2017-11-17 20:58:29 +0100

    refactor(AppModule): import from public_api rather than from an src/ file

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SweetAlert2Module } from '../../src/sweetalert2.module';
+import { SweetAlert2Module } from '../../public_api';
 import { AppComponent } from './app.component';
 
 @NgModule({
