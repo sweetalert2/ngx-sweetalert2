e959df1833d173eec1544984e4d5473fe8614123
Unknown <mtouverey@methodinthemadness.eu>
2017-11-20 17:17:26 +0100

    fix(*): change around how the library's packaging is done (#34)

--- public_api.ts
+++
@@ -1,6 +0,0 @@
-export { SwalPartialTargets } from './src/swal-partial-targets';
-export * from './src/swal-events';
-export { SwalComponent } from './src/swal.component';
-export { SwalDirective } from './src/swal.directive';
-export { SwalPartialDirective } from './src/swal-partial.directive';
-export { SweetAlert2Module } from './src/sweetalert2.module';
---
+++ src/public_api.ts
@@ -0,0 +1,6 @@
+export { SwalPartialTargets } from './swal-partial-targets';
+export * from './swal-events';
+export { SwalComponent } from './swal.component';
+export { SwalDirective } from './swal.directive';
+export { SwalPartialDirective } from './swal-partial.directive';
+export { SweetAlert2Module } from './sweetalert2.module';
