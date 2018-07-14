b76f89fdd307440940e254c942672a544fa488f3
Limon Monte <limon.monte@gmail.com>
2018-07-15 02:20:53 +0900

    chore(travis): enable CI tests, test in node 10 instead of node 9

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = config => {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['ChromeHeadless'],
         singleRun: false
     });
 };
