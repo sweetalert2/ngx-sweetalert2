f842ebcf4251398a10229e80ec54277781818499
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2019-08-18 00:43:59 +0200

    feat(SwalComponent): prepare the ground to welcome the future-old component

--- projects/ngx-sweetalert2/src/public-api.ts
+++ projects/ngx-sweetalert2/src/public-api.ts
@@ -1,3 +1,3 @@
-export * from './lib/ngx-sweetalert2.component';
+export * from './lib/swal.component';
 export * from './lib/sweetalert2.module';
 export { SwalModuleLoader } from './lib/sweetalert2-loader.service';
