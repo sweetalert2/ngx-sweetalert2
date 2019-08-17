957cdcc66845e6610c90cd487588b40d9939deb3
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2019-08-18 00:42:00 +0200

    feat: add ability to let the customer choose which sweetalert2 build to load

--- projects/ngx-sweetalert2/src/lib/sweetalert2.module.ts
+++ projects/ngx-sweetalert2/src/lib/sweetalert2.module.ts
@@ -1,10 +1,47 @@
-import { NgModule } from '@angular/core';
-import { NgxSweetalert2Component } from './ngx-sweetalert2.component';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { swalProviderToken } from './config';
+import { SwalComponent } from './swal.component';
+import { SwalProvider, SweetAlert2LoaderService } from './sweetalert2-loader.service';
+
+export interface Sweetalert2ModuleConfig {
+    provideSwal?: SwalProvider;
+}
+
+export function provideSwal() {
+    return import('sweetalert2');
+}
 
 @NgModule({
-    declarations: [NgxSweetalert2Component],
+    declarations: [SwalComponent],
     imports: [],
-    exports: [NgxSweetalert2Component]
+    exports: [SwalComponent]
 })
 export class SweetAlert2Module {
+    public static forRoot(options: Sweetalert2ModuleConfig = {}): ModuleWithProviders {
+        return {
+            ngModule: SweetAlert2Module,
+            providers: [
+                SweetAlert2LoaderService,
+                {
+                    provide: swalProviderToken,
+                    useValue: options.provideSwal || provideSwal
+                }
+            ]
+        };
+    }
+
+    public static forChild(options: Sweetalert2ModuleConfig = {}): ModuleWithProviders {
+        return {
+            ngModule: SweetAlert2Module,
+            providers: [
+                ...options.provideSwal ? [
+                    SweetAlert2LoaderService,
+                    {
+                        provide: swalProviderToken,
+                        useValue: options.provideSwal
+                    }
+                ] : []
+            ]
+        };
+    }
 }
