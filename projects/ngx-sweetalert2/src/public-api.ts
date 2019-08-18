895b4f15d206d140416a7595e3f231331d30a4e2
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2019-08-18 23:11:58 +0200

    feat(*): <swal> component is back, better than ever

--- projects/ngx-sweetalert2/src/public-api.ts
+++ projects/ngx-sweetalert2/src/public-api.ts
@@ -1,3 +1,4 @@
 export * from './lib/swal.component';
+export * from './lib/swal-events';
 export * from './lib/sweetalert2.module';
-export { SwalModuleLoader } from './lib/sweetalert2-loader.service';
+export { SwalModuleLoader, SweetAlert2LoaderService } from './lib/sweetalert2-loader.service';
