27a4da119475f877baa7ea4a2557b683f2f81c96
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2019-08-31 21:16:02 +0200

    docs(demo): basic examples

--- projects/ngx-sweetalert2-demo/src/app/nested/nested.module.ts
+++ projects/ngx-sweetalert2-demo/src/app/nested/nested.module.ts
@@ -3,9 +3,15 @@ import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { NestedRoutingModule } from './nested-routing.module';
 import { NestedComponent } from './nested.component';
 
+export function provideSwal() {
+    return import('sweetalert2').then(({ default: swal }) => swal.mixin({
+        backdrop: false
+    }));
+}
+
 @NgModule({
     imports: [
-        SweetAlert2Module.forChild(),
+        SweetAlert2Module.forChild({ provideSwal }),
         NestedRoutingModule
     ],
     declarations: [
