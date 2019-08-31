8bf9e90a2a3fdd9af484f3ba3aabc4ddf87a48ef
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2019-08-31 18:49:10 +0200

    feat: add ability to disable auto-dismissal of the modal on component destroy

--- projects/ngx-sweetalert2/src/lib/di.ts
+++ projects/ngx-sweetalert2/src/lib/di.ts
@@ -2,3 +2,5 @@ import { InjectionToken } from '@angular/core';
 import Swal from 'sweetalert2';
 
 export const swalProviderToken = new InjectionToken<typeof Swal>('@sweetalert2/ngx-sweetalert2#swalLibraryToken');
+
+export const dismissOnDestroyToken = new InjectionToken<typeof Swal>('@sweetalert2/ngx-sweetalert2#dismissOnDestroy');
