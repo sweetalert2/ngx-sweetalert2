e41c22919c516214f64c656728a2215a84f11cc9
Limon Monte <limon.monte@gmail.com>
2019-11-09 12:37:15 +0200

    chore: enable tests in CI

--- projects/ngx-sweetalert2/karma.conf.js
+++ projects/ngx-sweetalert2/karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const isCI = !!process.env.GITHUB_ACTION;
+const browsers = isCI ? ['ChromeHeadless'] : ['Chrome'];
+
 module.exports = function (config) {
     config.set({
         basePath: '',
@@ -25,7 +28,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers,
         singleRun: false,
         restartOnFileChange: true
     });
