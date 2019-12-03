988b621d6bdeb9c21350c378540c74a654babb90
Limon Monte <limonte@users.noreply.github.com>
2019-12-04 05:32:29 +0600

    chore: enable tests in CI (#141)
    * chore: enable tests in CI
    
    * chore(ci): ad 5 min timeout to CI build
    
    * tests: fix test lib tests not running
    
    * tests: add conf for CI tests and optimize CI pipeline (fail early, build what's necessary)
    
    * chore(package): upgrade & dedupe dependencies (cherry-picked from master)
    
    * chore: this doesn't seem to be needed...
    
    * chore(workflows): build sooner

--- projects/ngx-sweetalert2-demo/karma.conf.js
+++ projects/ngx-sweetalert2-demo/karma.conf.js
@@ -27,6 +27,12 @@ module.exports = function (config) {
         autoWatch: true,
         browsers: ['Chrome'],
         singleRun: false,
-        restartOnFileChange: true
+        restartOnFileChange: true,
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox', '--disable-gpu']
+            }
+        }
     });
 };
