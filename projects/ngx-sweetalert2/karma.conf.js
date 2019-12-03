8b2aaa9866cfd34663f2587ea9cbf2290f85736f
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2019-12-04 00:09:18 +0100

    tests: add conf for CI tests and optimize CI pipeline (fail early, build what's necessary)

--- projects/ngx-sweetalert2/karma.conf.js
+++ projects/ngx-sweetalert2/karma.conf.js
@@ -1,8 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const isCI = !!process.env.GITHUB_ACTION;
-const browsers = isCI ? ['ChromeHeadless'] : ['Chrome'];
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
     config.set({
@@ -28,8 +27,14 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers,
+        browsers: ['Chrome'],
         singleRun: false,
-        restartOnFileChange: true
+        restartOnFileChange: true,
+        customLaunchers: {
+            ChromeHeadlessCI: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox', '--disable-gpu']
+            }
+        }
     });
 };
