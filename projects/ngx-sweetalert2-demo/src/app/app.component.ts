6ad25bc64e3cf594a590f47f534446083462ff48
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2020-09-17 23:40:43 +0200

    docs: add an example showing how to use *swalPortal in the demo app

--- projects/ngx-sweetalert2-demo/src/app/app.component.ts
+++ projects/ngx-sweetalert2-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwalPortalTargets } from '@sweetalert2/ngx-sweetalert2';
 
 @Component({
     selector: 'demo-app',
@@ -9,4 +10,27 @@ export class AppComponent {
     public modalFireCondition = false;
 
     public isSwalVisible = false;
+
+    private readonly dynamicTextChunks = 'This dynamic content is controlled by Angular'.split(' ');
+
+    private dynamicTextChunksIntervalHandle?: number;
+
+    private currentTextChunkOffset = 0;
+
+    public constructor(public readonly targets: SwalPortalTargets) {
+    }
+
+    public get currentTextChunk(): string {
+        return this.dynamicTextChunks[this.currentTextChunkOffset % this.dynamicTextChunks.length];
+    }
+
+    public startDynamicTextRotation(): void {
+        this.currentTextChunkOffset = 0;
+
+        this.dynamicTextChunksIntervalHandle = setInterval(() => this.currentTextChunkOffset++, 1000);
+    }
+
+    public stopDynamicTextRotation(): void {
+        clearInterval(this.dynamicTextChunksIntervalHandle);
+    }
 }
