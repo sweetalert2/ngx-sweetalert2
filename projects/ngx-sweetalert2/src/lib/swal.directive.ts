7503dc76695a7ea24026abec9150493589d44981
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2020-09-21 22:50:36 +0200

    feat(SwalDirective): add (deny) @Output()

--- projects/ngx-sweetalert2/src/lib/swal.directive.ts
+++ projects/ngx-sweetalert2/src/lib/swal.directive.ts
@@ -49,7 +49,10 @@ export class SwalDirective implements OnInit, OnDestroy {
 
     /**
      * Emits when the user clicks "Confirm".
-     * Bears a value when using "input", resolved "preConfirm", etc.
+     * The event value ($event) can be either:
+     *  - by default, just `true`,
+     *  - when using {@link input}, the input value,
+     *  - when using {@link preConfirm}, the return value of this function.
      *
      * Example:
      *     <swal (confirm)="handleConfirm($event)"></swal>
@@ -62,9 +65,24 @@ export class SwalDirective implements OnInit, OnDestroy {
     public readonly confirm = new EventEmitter<any>();
 
     /**
+     * Emits when the user clicks "Deny".
+     * This event bears no value.
+     * Use `(deny)` (along with {@link showDenyButton}) when you want a modal with three buttons (confirm, deny and
+     * cancel), and/or when you want to handle clear refusal in a separate way than simple dismissal.
+     *
+     * Example:
+     *     <swal (deny)="handleDeny()"></swal>
+     *
+     *     public handleDeny(): void {
+     *     }
+     */
+    @Output()
+    public readonly deny = new EventEmitter<void>();
+
+    /**
      * Emits when the user clicks "Cancel", or dismisses the modal by any other allowed way.
-     * By default, it will emit a string representing the reason for which the SweetAlert has been closed.
-     * The reason is `undefined` when {@link dismiss} is called.
+     * The event value ($event) is a string that explains how the modal was dismissed. It is `undefined` when
+     * the modal was programmatically closed (through {@link dismiss} for example).
      *
      * Example:
      *     <swal (cancel)="handleCancel($event)"></swal>
@@ -145,6 +163,7 @@ export class SwalDirective implements OnInit, OnDestroy {
         const swalClosed = new Subject();
 
         this.swalInstance.confirm.asObservable().pipe(takeUntil(swalClosed)).subscribe(v => this.confirm.emit(v));
+        this.swalInstance.deny.asObservable().pipe(takeUntil(swalClosed)).subscribe(v => this.deny.emit(v));
         this.swalInstance.cancel.asObservable().pipe(takeUntil(swalClosed)).subscribe(v => this.cancel.emit(v));
 
         this.swalInstance.fire().then(() => swalClosed.next());
