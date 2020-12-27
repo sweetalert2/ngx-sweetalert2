e9905447e114fbd90a9d8f50a953b71ed78154db
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2020-12-27 16:07:48 +0100

    feat: upgrade to Angular 11

--- projects/ngx-sweetalert2-demo/src/app/app.component.ts
+++ projects/ngx-sweetalert2-demo/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
 
     private readonly dynamicTextChunks = 'This dynamic content is controlled by Angular'.split(' ');
 
-    private dynamicTextChunksIntervalHandle?: number;
+    private dynamicTextChunksIntervalHandle?: any;
 
     private currentTextChunkOffset = 0;
 
--- projects/ngx-sweetalert2/src/test.ts
+++ projects/ngx-sweetalert2/src/test.ts
@@ -1,7 +1,7 @@
-import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
+import { getTestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: any;
 
