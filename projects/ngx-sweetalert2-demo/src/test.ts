05263998ab7823b0a3359e7ee5c018859a27422b
Morgan Touverey Quilling <mtouverey@methodinthemadness.eu>
2020-12-27 16:07:48 +0100

    feat: upgrade to Angular 11

--- projects/ngx-sweetalert2-demo/src/test.ts
+++ projects/ngx-sweetalert2-demo/src/test.ts
@@ -1,6 +1,6 @@
+import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/dist/zone-testing';
 
 declare const require: any;
 
