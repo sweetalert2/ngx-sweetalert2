ac698d3d90cda7ac153adec8d8b0f66850871ef0
Limon Monte <limonte@users.noreply.github.com>
2022-12-09 12:00:43 +0200

    feat: add Angular 15 support

--- projects/ngx-sweetalert2/src/test.ts
+++ projects/ngx-sweetalert2/src/test.ts
@@ -9,9 +9,3 @@ declare const require: any;
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
     teardown: { destroyAfterEach: false }
 });
-
-//=> Then we find all the tests
-const context = require.context('./', true, /\.spec\.ts$/);
-
-//=> And load the modules
-context.keys().map(context);
